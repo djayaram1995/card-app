6e15079913433cd485c0fb7b1c8d2d1c3aa5a608
JAYARAM D <34568818+djayaram1995@users.noreply.github.com>
2019-05-09 13:14:01 +0530

    modify listItem

--- src/App.js
+++ src/App.js
@@ -4,144 +4,149 @@ import AddModal from "./components/AddModal";
 //import CarouselComponent from './components/carousel'
 //import Label from './label'
 import "./App.css";
-const items = [
-  {
-    tabKey: 1,
-    cardsData: [
-      {
-        key: 1,
-        cardData: [
-          {
-            keyData: 1,
-            imageUrl:
-              "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
-            cardText: "This is a bulb"
-          },
-          {
-            keyData: 2,
-            imageUrl:
-              "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
-            cardText: "This is a latest invention please check for issues"
-          }
-        ]
-      },
-      {
-        key: 2,
-        cardData: [
-          {
-            keyData: 1,
-            imageUrl:
-              "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
-            cardText: "Finished bulb drawing"
-          },
-          {
-            keyData: 2,
-            imageUrl:
-              "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
-            cardText: "Imported the image"
-          },
-          {
-            keyData: 3,
-            imageUrl:
-              "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
-            cardText: "Done building"
-          }
-        ]
-      }
-    ]
-  },
-  {
-    tabKey: 2,
-    cardsData: [
-      {
-        key: 1,
-        cardData: [
-          {
-            keyData: 1,
-            imageUrl:
-              "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
-            cardText: "This is a bulb"
-          },
-          {
-            keyData: 2,
-            imageUrl:
-              "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
-            cardText: "This is a latest invention please check for issues"
-          }
-        ]
-      },
-      {
-        key: 2,
-        cardData: [
-          {
-            keyData: 1,
-            imageUrl:
-              "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
-            cardText: "Finished bulb drawing"
-          },
-          {
-            keyData: 2,
-            imageUrl:
-              "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
-            cardText: "Imported the image"
-          },
-          {
-            keyData: 3,
-            imageUrl:
-              "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
-            cardText: "Done building"
-          }
-        ]
-      }
-    ]
-  },
-  {
-    tabKey: 3,
-    cardsData: [
-      {
-        key: 1,
-        cardData: [
-          {
-            keyData: 1,
-            imageUrl:
-              "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
-            cardText: "This is a bulb"
-          },
-          {
-            keyData: 2,
-            imageUrl:
-              "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
-            cardText: "This is a latest invention please check for issues"
-          }
-        ]
-      },
-      {
-        key: 2,
-        cardData: [
-          {
-            keyData: 1,
-            imageUrl:
-              "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
-            cardText: "Finished bulb drawing"
-          },
-          {
-            keyData: 2,
-            imageUrl:
-              "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
-            cardText: "Imported the image"
-          },
-          {
-            keyData: 3,
-            imageUrl:
-              "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
-            cardText: "Done building"
-          }
-        ]
-      }
-    ]
+
+var response = {
+  "Count": 14,
+  "listitem": [
+    {
+      "view_count": 0,
+      "listitem_type": "text",
+      "listitem_id": 1,
+      "listitem_content": "postman_21_5_listitem_53_updated_1_0707",
+      "listitem_details": "",
+      "is_favourite": "N"
+    },
+    {
+      "view_count": 0,
+      "listitem_type": "image",
+      "listitem_id": 2,
+      "listitem_content": "postman_21_5_listitem_53_updated_1",
+      "listitem_details": "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
+      "is_favourite": "N"
+    },
+    {
+      "view_count": 0,
+      "listitem_type": "video",
+      "listitem_id": 4,
+      "listitem_content": "postman_21_5_listitem_53_updated_1",
+      "listitem_details": "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
+      "is_favourite": "N"
+    },
+    {
+      "view_count": 0,
+      "listitem_type": "doc",
+      "listitem_id": 5,
+      "listitem_content": "postman_21_5_listitem_53_updated_1",
+      "listitem_details": "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
+      "is_favourite": "Y"
+    },
+    {
+      "view_count": 0,
+      "listitem_type": "image",
+      "listitem_id": 6,
+      "listitem_content": "postman_21_5_listitem_53_added_2",
+      "listitem_details": "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
+      "is_favourite": "N"
+    },
+    {
+      "view_count": 0,
+      "listitem_type": "image",
+      "listitem_id": 7,
+      "listitem_content": "postman_21_5_listitem_53_added_2",
+      "listitem_details": "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
+      "is_favourite": "Y"
+    },
+    {
+      "view_count": 0,
+      "listitem_type": "image",
+      "listitem_id": 8,
+      "listitem_content": "postman_21_5_listitem_53_added_2_updated_02",
+      "listitem_details": "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
+      "is_favourite": "Y"
+    },
+    {
+      "view_count": 0,
+      "listitem_type": "image",
+      "listitem_id": 11,
+      "listitem_content": "Specification_list",
+      "listitem_details": "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
+      "is_favourite": "Y"
+    },
+    {
+      "view_count": 0,
+      "listitem_type": "image",
+      "listitem_id": 12,
+      "listitem_content": "Specification_list_02",
+      "listitem_details": "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
+      "is_favourite": "Y"
+    },
+    {
+      "view_count": 0,
+      "listitem_type": "text",
+      "listitem_id": 103,
+      "listitem_content": "1. use the api response to fetch the updated text and edit the array/state in browser",
+      "listitem_details": "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
+      "is_favourite": "N"
+    },
+    {
+      "view_count": 0,
+      "listitem_type": "text",
+      "listitem_id": 104,
+      "listitem_content": "1. use the api response to fetch the updated is_favourite flag and view_count, and edit the array/state in browser",
+      "listitem_details": "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
+      "is_favourite": "N"
+    },
+    {
+      "view_count": 0,
+      "listitem_type": "text",
+      "listitem_id": 105,
+      "listitem_content": "1. to make all the cards of same size",
+      "listitem_details": "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
+      "is_favourite": "N"
+    },
+    {
+      "view_count": 0,
+      "listitem_type": "text",
+      "listitem_id": 106,
+      "listitem_content": "1. Username and Email are required paramters.",
+      "listitem_details": "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
+      "is_favourite": "N"
+    },
+    {
+      "view_count": 1,
+      "listitem_type": "text",
+      "listitem_id": 107,
+      "listitem_content": "2. Django will consider email as username and same is required at the time of login. Also a new user can not use the email id that has been used by an already registered user since email is treated as user name and it should be unique.",
+      "listitem_details": "https://www.feit.com/wp-content/uploads/2017/04/Aclearfilamant-800x570.jpg",
+      "is_favourite": "N"
+    }
+  ]
+}
+const favItem = [];
+const normalItem = [];
+response.listitem.map((item, index) => {
+  if (item.is_favourite === "Y") {
+    favItem.push(item)
+    
+  } else {
+    normalItem.push(item)
   }
-  
-];
+  return null;
+})
+const fav= [];
+const norm= [];
+while(favItem.length > 0) {
+  fav.push(favItem.splice(0,4));
+}
+while(normalItem.length > 0) {
+  norm.push(normalItem.splice(0,4));
+}
+const listItem = [{
+  tabKey: 1, cardsData: fav
+},{
+  tabKey: 2, cardsData: norm
+},
+]
 class App extends Component {
   constructor() {
     super();
@@ -149,7 +154,7 @@ class App extends Component {
       addModal: false
     };
   }
-  
+
   addClick = () => {
     this.setState({
       addModal: true
@@ -161,9 +166,10 @@ class App extends Component {
     });
   };
   render() {
+    console.log(listItem);
     return (
       <div className="App">
-        <TabComponent items={items} />
+        <TabComponent items={listItem} />
         <h1 id="fixedAdd" onClick={this.addClick}>
           +
         </h1>
